8e12f81b9f87b516aebda8acd9dbe0993ad06268
Edholm92 <gusedhhe@student.gu.se>
2018-11-27 19:19:27 +0100

    Initial commit

---
+++ app/src/main/java/com/tig167/spotifystatistics/spotifystatistics/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tig167.spotifystatistics.spotifystatistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
