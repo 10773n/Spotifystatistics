dc2949a850628638abd50e32a58a538ce14ecf69
10773n <lotten.bristav@hotmail.com>
2018-11-30 19:04:20 +0100

    Log in acitivity skapad

---
+++ app/src/main/java/com/tig167/spotifystatistics/spotifystatistics/LogInActvity.java
@@ -0,0 +1,13 @@
+package com.tig167.spotifystatistics.spotifystatistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
