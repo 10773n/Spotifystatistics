8bc3095377d5580f7c5a5875f8c30f78ff62a545
10773n <lotten.bristav@hotmail.com>
2018-12-01 16:01:04 +0100

    Länkade log in-knappen och aktivitet tab1

--- app/src/main/java/com/tig167/spotifystatistics/spotifystatistics/LogInActvity.java
+++ app/src/main/java/com/tig167/spotifystatistics/spotifystatistics/LogInActvity.java
@@ -1,13 +1,28 @@
 package com.tig167.spotifystatistics.spotifystatistics;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActvity extends AppCompatActivity {
+
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        login_button = (Button)findViewById(R.id.Blogin);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogInActvity.this, TabbedActivity.class));
+            }
+        });
+
     }
 }
